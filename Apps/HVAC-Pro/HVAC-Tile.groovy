ef8df0d35ecec2876741079d01cd682807c85f42
parish99 <parish99@hotmail.com>
2020-06-11 00:10:27 -0500

    Update HVAC-Tile.groovy

--- Apps/HVAC-Pro/HVAC-Tile.groovy
+++ Apps/HVAC-Pro/HVAC-Tile.groovy
@@ -9,28 +9,19 @@ metadata {
         capability "IlluminanceMeasurement"
         capability "FanControl"
         capability "Thermostat"
-        
-/*        //Commands
-        command "setTargetTemp", ["NUMBER"]
-        command "setVentLevel", ["NUMBER"]
-        command "setLightLevel", ["NUMBER"]
-        command "setHumidLevel", ["NUMBER"]
-        command "setTempLevel", ["NUMBER"]       
-        command "setShadeLevel", ["NUMBER"]
-        command "setMotion", ["ENUM"]
-        command "setMode", ["STRING"]
-        command "setFan", ["NUMBER"]
-        command "refresh"
-*/        
+            
         //Attributes
         attribute "targetTemp", 'Number'
+        attribute "ventSetpoint", 'Number'
         attribute "ventPosition", 'Number'
         attribute "illuminance", 'Number'
         attribute "humidity", 'Number'
         attribute "temperature", 'Number'
         attribute "shadePosition", 'Number'
-        attribute "presence", 'enum', ['Occupied','Unoccupied']
-        attribute "mode", 'string'
+        attribute "presence", 'enum', ['occupied','unoccupied']
+        attribute "motion", 'enum', ['active','inactive']
+        attribute "tMode", 'ENUM', ['auto', 'off', 'heat', 'emergency heat', 'cool']
+        attribute "tState", 'ENUM', ["heating", "pending cool", "pending heat", "vent economizer", "idle", "cooling", "fan only"]
         attribute "speed", 'enum', ["low","medium-low","medium","medium-high","high","on","off","auto"]
         attribute "tileData", "string"
         attribute "message", "string"
@@ -44,20 +35,8 @@ metadata {
 
 def installed() {
     log.warn "installed..."
-    setValues("Occupied","Off",0,0,0,0,"Off",0,0,"Message")
-    /*
-    setTargetTemp(0)
-    setVentLevel(0)
-    setLightLevel(0)
-    setHumidLevel(0.0)
-    setTempLevel(0.0)
-    setShadeLevel(0)
-    setFanLevel(0)
-    setMotion('Unoccupied')
-    setMode('Inactive')
-*/
+    setValues("Occupied","Inactive","Off","Idle",0,0,0,0,0,"Off",0,0,"Message")
     sendEvent(name: "tileData", value: "Waiting For Update")
-    //sendEvent(name: "presence", value: 'Occupied')
 }
 
 def updated() {
@@ -67,30 +46,33 @@ def updated() {
     installed()
 }
 
-//def parse(String description) {
-//}
+//(state.occupied,state.Motion,state.HVACmode,state.HVACstate,state.roomSetPoint,state.currentTemperature,state.currentHumidity,state.ventSetPoint,state.currentVentLevel,state.fanSpeed,state.currentShadeLevel,state.currentLux,state.msg)
 
-
-//////////////////////////////////////////////////////////////////////////////////
-//(state.Motion,state.HVACmode,state.roomSetPoint,state.currentTemperature,state.currentHumidity,state.currentVentLevel,state.speed,state.currentShadeLevel,state.currentLux,Message)
-def setValues(os,sm,tt,tl,hl,vl,sf,sl,ll,msg){
+def setValues(os,mt,sm,ss,tt,tl,hl,vsp,vl,sf,sl,ll,msg){
     sendEvent(name: "targetTemp", value: tt, unit: "%")
     sendEvent(name: "ventPosition", value: vl, unit: "%")
+    sendEvent(name: "ventSetpoint", value: vsp, unit: "%")
     sendEvent(name: "illuminance", value: ll, unit: "Lux")
     sendEvent(name: "humidity", value: hl, unit: "%")
     sendEvent(name: "temperature", value: tl, unit: "F")
     sendEvent(name: "shadePosition", value: sl, unit: "%")
     sendEvent(name: "presence", value: os, unit: "State")
-    sendEvent(name: "mode", value: sm, unit: " Mode")
+    sendEvent(name: "motion", value: mt, unit: "State")
+    sendEvent(name: "tMode", value: sm, unit: " Mode")
+    sendEvent(name: "tState", value: ss, unit: " State")
     sendEvent(name: "speed", value: sf, unit: "Speed")
     sendEvent(name: "message", value: msg)
     
     def map=[:]
+    if (txtEnable) log.info "${os},${mt},${sm},${ss}"
     map.Presence=os
-    map.Mode=sm.toLowerCase().capitalize()
+    map.Motion=mt
+    map.tMode=sm.toLowerCase().capitalize()
+    map.tState=ss.toLowerCase().capitalize()
     map.Target=tt
     map.Temperature=tl
     map.Humidity=hl
+    map.VentSP=vsp
     map.VentPos=vl
     map.FanSpeed=sf
     map.ShadePos=sl
@@ -100,29 +82,29 @@ def setValues(os,sm,tt,tl,hl,vl,sf,sl,ll,msg){
     if (txtEnable) log.info "${map}"
     
     def tileDat = ""+"<br>"
-    if (map.Presence != null) tileDat += "Presence:          "+map.Presence+"<br>"
-    tileDat += "HVAC:                     "+map.Mode+"<br>"
-    tileDat += "Target Temp:      "+map.Target+"°F"+"<br>"
-    tileDat += "Temperature:    "+map.Temperature+"°F"+"<br>"
-    if (map.Humidity != null) tileDat += "Humidity:         "+map.Humidity+"%"+"<br>"
-    tileDat += "Vent Position:     "+map.VentPos+"%"+"<br>"
-    if (map.FanSpeed != null) tileDat += "Fan Speed:                "+map.FanSpeed+"<br>"
-    tileDat += "Message:      "+map.Msg+"<br>"
     
-    //tileDat = "<div style='color: black; height: 90%; background-color: green; font-size: 15px; white-space: pre-wrap; position: absolute; left: 0; top: 10%; width: 130%;'><div style='position: absolute;top: 50%;left: 35%;transform: translate(-50%, -50%);'${tileDat}</div></div>"
+    tileDat += device.label+"<br>"
+    tileDat += "<br>"
+    tileDat += "HVAC: "+map.tMode+" and "+map.tState+"<br>"
+    //tileDat += "Temp SP: "+map.Target+"°F"+"<br>"
+    tileDat += "Temp: (SP "+map.Target+"-PV "+map.Temperature+")°F"+"<br>"
+    //tileDat += "Temp PV:    "+map.Temperature+"°F"+"<br>"
+    if (map.Humidity != null) tileDat += "Humidity:         "+map.Humidity+"%"+"<br>"
+    tileDat += "Vent: (SP "+map.VentSP+"-"+"PV "+map.VentPos+")%"+"<br>"
+    if (map.FanSpeed != null) tileDat += "Fan Speed:                "+map.FanSpeed+"<br>"
+    if (map.Lux != null) tileDat += "Light:                "+map.Lux+" lux"+"<br>"
+    if (map.Presence != null) tileDat += "Presence:          "+map.Presence+"<br>"
+    tileDat += "Status:      "+map.Msg+"<br>"
+    
+    //tileDat = "<div style='background-color: red;'${tileDat}</div>"
+    if(map.Motion == "Active")tileDat = "<div style='color: black; height: 140%; background-color: #ffcc00; font-size: 18px; position: absolute; left: 0; top: -20%; width: 130%;'><div style='position: absolute;top: 45%;left: 39%;transform: translate(-50%, -50%);'${tileDat}</div></div>"
+    else if(map.tMode == "Heat")tileDat = "<div style='color: white; height: 140%; background-color: #990000; font-size: 18px; position: absolute; left: 0; top: -20%; width: 130%;'><div style='position: absolute;top: 45%;left: 39%;transform: translate(-50%, -50%);'${tileDat}</div></div>"
+    else if(map.tMode == "Cool")tileDat = "<div style='color: white; height: 140%; background-color: #0066cc; font-size: 18px; position: absolute; left: 0; top: -20%; width: 130%;'><div style='position: absolute;top: 45%;left: 39%;transform: translate(-50%, -50%);'${tileDat}</div></div>"
+    else if(map.Msg == "Offline")tileDat = "<div style='color: black; height: 140%; background-color: #9900cc; font-size: 18px; position: absolute; left: 0; top: -20%; width: 130%;'><div style='position: absolute;top: 45%;left: 39%;transform: translate(-50%, -50%);'${tileDat}</div></div>"
+    else tileDat = "<div style='color: black; height: 140%; background-color: white; font-size: 18px; position: absolute; left: 0; top: -20%; width: 130%;'><div style='position: absolute;top: 45%;left: 39%;transform: translate(-50%, -50%);'${tileDat}</div></div>"
     
     sendEvent(name: "tileData", value: tileDat, isStateChange: true, displayed: true)
 }
-
-
-
-
-
-
-
-
-
-
 
 
 /*
@@ -182,23 +164,4 @@ def setFan(sf) {
     sendEvent(name: "fanSpeed", value: sf, unit: "Level", descriptionText: descriptionText)
 }
 
-
-
-def updateDisplayTile(){
-    sendEvent(name: "message", value: "Vent Online")
-    def tileDat = ""+"<br>"
-    tileDat += "Presence:          "+device.currentValue("presence")+"<br>"
-    tileDat += "Mode:                     "+(device.currentValue("mode").toLowerCase().capitalize())+"<br>"
-    tileDat += "Target Temp:      "+device.currentValue("targetTemp")+"°F"+"<br>"
-    tileDat += "Temperature:    "+device.currentValue("temperature")+"°F"+"<br>"
-    tileDat += "Humidity:         "+device.currentValue("humidity")+"%"+"<br>"
-    tileDat += "Vent Position:     "+device.currentValue("ventPosition")+"%"+"<br>"
-    tileDat += "Fan Speed:                "+device.currentValue("fanSpeed")+"<br>"
-    tileDat += "Message:      "+device.currentValue("message")+"<br>"
-    
-    //tileDat = "<div style='color: black; height: 90%; background-color: green; font-size: 15px; white-space: pre-wrap; position: absolute; left: 0; top: 10%; width: 130%;'><div style='position: absolute;top: 50%;left: 35%;transform: translate(-50%, -50%);'${tileDat}</div></div>"
-    
-    sendEvent(name: "tileData", value: tileDat, isStateChange: true, displayed: true)
-
-}
 */
